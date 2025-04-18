802f10789ca2aa326430025ef1b1f61715d2d6b5
Patel purv <patelpurv2643@gmail.com>
2025-04-18 16:09:54 +0530

    bug fixing

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Hero from '../Home/Hero'
 
-const Home = () => {
+
+function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <Hero/>
+    </div>
   )
 }
 
