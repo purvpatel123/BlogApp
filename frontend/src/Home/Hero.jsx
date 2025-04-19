a2ea6a1493385c2963e1fe72c846c508c45070ea
Patel purv <patelpurv2643@gmail.com>
2025-04-19 22:17:15 +0530

    some changes in file

--- frontend/src/Home/Hero.jsx
+++ frontend/src/Home/Hero.jsx
@@ -8,12 +8,12 @@ function Hero() {
   return (
     <div className='container mx-auto my-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 p-6'>
       {blogs && blogs.length>0 ?(
-        blogs.slice(3,8).map((element)=>{
+        blogs.slice(0,4).map((element)=>{
           return(
             <Link to={`/`} key={element._id}
             className='bg-white rounded-lg hover:shadow-lg overflow-hidden transform
              hover:scale-105 transition-transform duration-300'>
-              <div className='relative'>
+              <div className='group relative'>
                 <img src={element.blogImage.url} alt=""
                  className='w-full h-56 object-cover'/>
                 <div className='absolute inset-0 bg-gradient-to-t from-black via-transparent to-transparent opacity-75 group-hover:opacity-100 transition-transform duration-300'></div>
@@ -21,7 +21,7 @@ function Hero() {
                 transition-colors duration-300'>{element.title}</h1>
               </div>
               <div className='p-6 flex items-center'>
-                <img src={element.adminPhoto} alt="" className='w-12 h-12 rounded-full border-2 border-yellow-400' />
+                <img src={element.adminPhoto  } alt="" className='w-12 h-12 rounded-full border-2 border-yellow-400' />
                 <div className='ml-4'>
                   <p className='text-lg font-semibold text-gray-800'>{element.adminName}</p>
                   <p className='text-xs text-gray-400'>New</p>
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Hero from '../Home/Hero'
-
+import Trending from '../Home/Trending'
+import Devotional from '../Home/Devotional'
 
 function Home() {
   return (
     <div>
       <Hero/>
+      <Trending/>
+      <Devotional/>
     </div>
   )
 }
