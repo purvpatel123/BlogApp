604fd90ece0946329dc815db265c9bd29c6cd1b6
Patel purv <patelpurv2643@gmail.com>
2025-04-21 22:00:52 +0530

    cretor page completed

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import Hero from '../Home/Hero'
 import Trending from '../Home/Trending'
 import Devotional from '../Home/Devotional'
-
+import Creator from '../Home/Creator'
 function Home() {
   return (
     <div>
       <Hero/>
       <Trending/>
       <Devotional/>
+      <Creator/>
     </div>
   )
 }
