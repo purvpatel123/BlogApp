f9a52048eba72802232744c2daf10a4af57de4b1
Patel purv <patelpurv2643@gmail.com>
2025-06-03 10:20:07 +0530

    bug fixing

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,5 +1,3 @@
-// export const BACKEND_URL = "https://blogapp-1-88un.onrender.com" || "http://localhost:4001";
-
 export const BACKEND_URL =
   process.env.NODE_ENV === "production"
     ? "https://blogapp-1-88un.onrender.com"
