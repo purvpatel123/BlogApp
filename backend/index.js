facdd46304ed1477dec03aec4925699140f91d6a
Patel purv <patelpurv2643@gmail.com>
2025-06-03 11:13:49 +0530

    bug solve

--- backend/index.js
+++ backend/index.js
@@ -22,22 +22,31 @@ const MONGO_URL = process.env.MONGO_URI
     
 // }));
 
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://blogapp-2-zxnq.onrender.com",
-];
+app.use(
+   cors({
+     origin: 
+     "https://blogapp-2-zxnq.onrender.com",   
+     credentials: true,               
+    
+ }));
 
-app.use(cors({
-  origin: function (origin, callback) {
-    // Allow requests with no origin (like mobile apps or curl)
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true,
-}));
+
+// const allowedOrigins = [
+//   "http://localhost:5173",
+//   "https://blogapp-2-zxnq.onrender.com",
+// ];
+
+// app.use(cors({
+//   origin: function (origin, callback) {
+//     // Allow requests with no origin (like mobile apps or curl)
+//     if (!origin || allowedOrigins.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   },
+//   credentials: true,
+// }));
 
 
 app.use(express.json());
